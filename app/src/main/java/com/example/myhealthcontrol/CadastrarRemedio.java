bbab0465be12691364caba01ba771e7030a19700
Joao Vinicius <joviusdsg@gmail.com>
2023-01-21 13:35:17 -0300

    Design das Telas Completo

--- app/src/main/java/com/example/myhealthcontrol/CadastrarRemedio.java
+++ app/src/main/java/com/example/myhealthcontrol/CadastrarRemedio.java
@@ -10,5 +10,6 @@ public class CadastrarRemedio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar);
+        getSupportActionBar().hide();
     }
 }
